8fd6dd1baf431fad05da158fea0a7129e51337a7
PLM-18 <philemonmuleya@gmail.com>
2025-02-21 11:15:45 +0200

    Adding StoreData function

--- DeamonCRUD/main.go
+++ DeamonCRUD/main.go
@@ -3,7 +3,28 @@ package main
 import{
 	"fmt",
 	"log",
-	"os"
+	"os",
+	"net/http",
+	"encoding/json"
+}
+
+type Address struct{
+	Street string `json:"street"`
+	City string `json:"city"`
+	Zip string `json:"zip"`
+}
+
+type User struct{
+	ID string `json:"_id"`
+	Name string `json:"name"`
+	Age int `json:"age"`
+	Email string `json:"email"`
+	Address Address `json:"address"`
+}
+
+// Store data sends json data to the daemon
+func StoreData(url string, user User) error {
+	
 }
 
 func main(){
