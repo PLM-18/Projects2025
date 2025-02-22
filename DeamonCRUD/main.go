0f578f16a0ffafe9164008d508640072c19ddd80
PLM-18 <philemonmuleya@gmail.com>
2025-02-23 00:15:28 +0200

    starting CLI practice, I'll finish this in the morning

--- DeamonCRUD/main.go
+++ DeamonCRUD/main.go
@@ -24,7 +24,23 @@ type User struct{
 
 // Store data sends json data to the daemon
 func StoreData(url string, user User) error {
-	
+	jsonData, err := json.Marshal(user)
+	if err != nil {
+		return fmt.Errorf("Error marshalling json data: %v", err)
+	}
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("Error posting data: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Error posting data: %v", err)
+	}
+
+	fmt.Println("Data stored successfully")
+	return nil
 }
 
 func main(){
