920df9c31fd53719a2fb2a5eb035a8256a1727d5
PLM-18 <philemonmuleya@gmail.com>
2025-03-30 21:04:16 +0200

    Adding pages

---
+++ filemanager/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Home view works</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #333;
+}
+</style>
