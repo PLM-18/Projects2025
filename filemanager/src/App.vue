d980a702af1b7a02ca726c6c09910cfd08abf30e
PLM-18 <philemonmuleya@gmail.com>
2025-04-08 17:40:26 +0200

    Adding router

--- filemanager/src/App.vue
+++ filemanager/src/App.vue
@@ -1,27 +1,52 @@
 <template>
   <div id="app">
-    <Home />
+    <nav>
+      <router-link to="/" class="nav-link">Home</router-link>
+      <router-link to="/schema" class="nav-link">Schema View</router-link>
+      <router-link to="/xml" class="nav-link">XML View</router-link>
+    </nav>
+
+    <main>
+      <router-view></router-view>
+    </main>
   </div>
 </template>
 
 <script>
-import Home from './components/Home.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Home
-  }
+  name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+
+nav {
+  padding: 1rem;
+  background-color: #f8f9fa;
+  border-bottom: 1px solid #e9ecef;
+}
+
+.nav-link {
+  color: #2c3e50;
+  text-decoration: none;
+  font-weight: bold;
+  margin-right: 1rem;
+}
+
+.router-link-active {
+  text-decoration: underline;
+  color: #007bff;
+}
+
+main {
+  padding: 2rem;
 }
 </style>
