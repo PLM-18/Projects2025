6a20f63b96f29bd1c16e249dd50442a42f4c0b98
PLM-18 <philemonmuleya@gmail.com>
2025-07-27 22:39:50 +0200

    Latest app.jav achanges

--- objectdb/src/main/java/com/objectdbapp/App.java
+++ objectdb/src/main/java/com/objectdbapp/App.java
@@ -1,13 +1,18 @@
 package com.objectdbapp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.objectdbapp.model.Person;
+
+public class App {
+    private static final String PERSISTENCE_UNIT_NAME = "objectdb-test";
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+
+        try {
+
+        } catch (Exception e) {
+
+        }
     }
 }
