c5ce5dbc3d9c7e59eb428569ca0fc32e3c5dc9ba
PLM-18 <philemonmuleya@gmail.com>
2025-07-27 22:36:28 +0200

    feat: add Person entity and persistence configuration for ObjectDB

---
+++ objectdb/src/main/java/com/objectdbapp/Person.java
@@ -0,0 +1,50 @@
+package com.objectdbapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // marking as a JPA entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENDITY)
+    private Long id;
+
+    private String name;
+    private int age;
+
+    public Person() {
+
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name='" + name + '\'' + ", age=" + age + '}';
+    }
+}
